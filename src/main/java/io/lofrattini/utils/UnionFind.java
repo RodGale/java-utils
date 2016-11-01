48139ad0c9c531ec16884f390a835a19203a8b49
eroispaziali <lorenzo.frattini@gmail.com>
2016-11-01 14:14:31 +0000

    javadoc and unit test update

--- src/main/java/io/lofrattini/utils/UnionFind.java
+++ src/main/java/io/lofrattini/utils/UnionFind.java
@@ -7,6 +7,13 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A template implementation of the Union-Find set data structure.
+ * {@link https://en.wikipedia.org/wiki/Disjoint-set_data_structure}
+ * @author Lorenzo Frattini
+ *
+ * @param <T> the generic item type
+ */
 public class UnionFind<T> {
 	
 	private Map<T, Set<T>> itemMap = new HashMap<T, Set<T>>();
@@ -15,6 +22,12 @@ public class UnionFind<T> {
 		super();
 	}
 	
+	/**
+	 * Tells if there is a connection, either direct or indirect, between two items in the data structure
+	 * @param a the first item
+	 * @param b the second item
+	 * @return true if there is a connection, false otherwise
+	 */
 	public boolean isConnected(T a, T b) {
 		if (a!=null && b!=null) {
 			Set<T> aSet = itemMap.getOrDefault(a, new HashSet<T>(Arrays.asList(a)));
@@ -24,6 +37,12 @@ public class UnionFind<T> {
 		}
 	}
 	
+	/**
+	 * Create a links between two elements. 
+	 * If the elements don't exist yet they are added to the data structure
+	 * @param a the first element 
+	 * @param b the second element
+	 */
 	public void connect(T a, T b) {
 		if (a!=null && b!=null) {
 			Set<T> aSet = itemMap.getOrDefault(a, new HashSet<T>(Arrays.asList(a)));
--- src/test/java/io/lofrattini/utils/UnionFindTest.java
+++ src/test/java/io/lofrattini/utils/UnionFindTest.java
@@ -2,19 +2,13 @@ package io.lofrattini.utils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import io.lofrattini.utils.UnionFind;
-
 public class UnionFindTest {
 
-	public UnionFindTest() {
-		super();
-	}
-	
 	@Test
 	public void testWhenElementsDontExists() {
 		UnionFind<Integer> uf = new UnionFind<Integer>();
-		Assert.assertFalse(uf.isConnected(1, 2));
-		Assert.assertFalse(uf.isConnected(2, 1));
+		Assert.assertFalse("No connection expected", uf.isConnected(1, 2));
+		Assert.assertFalse("No connection expected", uf.isConnected(2, 1));
 	}
 	
 	@Test
@@ -22,17 +16,17 @@ public class UnionFindTest {
 		UnionFind<Integer> uf = new UnionFind<Integer>();
 		uf.connect(1, 2);
 		uf.connect(3, 4);
-		Assert.assertFalse(uf.isConnected(1, 3));
-		Assert.assertFalse(uf.isConnected(1, 4));
-		Assert.assertFalse(uf.isConnected(2, 3));
-		Assert.assertFalse(uf.isConnected(2, 4));
+		Assert.assertFalse("No connection expected", uf.isConnected(1, 3));
+		Assert.assertFalse("No connection expected", uf.isConnected(1, 4));
+		Assert.assertFalse("No connection expected", uf.isConnected(2, 3));
+		Assert.assertFalse("No connection expected", uf.isConnected(2, 4));
 	}
 	
 	@Test
 	public void testWhenSomeNullValues() {
 		UnionFind<Integer> uf = new UnionFind<Integer>();
-		Assert.assertFalse(uf.isConnected(1, null));
-		Assert.assertFalse(uf.isConnected(null, 1));
+		Assert.assertFalse("No connection expected with null values", uf.isConnected(1, null));
+		Assert.assertFalse("No connection expected with null values", uf.isConnected(null, 1));
 	}
 	
 	@Test
@@ -46,15 +40,22 @@ public class UnionFindTest {
 	@Test
 	public void testWhenAllValuesAreNull() {
 		UnionFind<Integer> uf = new UnionFind<Integer>();
-		Assert.assertFalse(uf.isConnected(null, null));
+		Assert.assertFalse("No connection expected with null values", uf.isConnected(null, null));
 	}
 	
 	@Test
 	public void testWhenDirectConnectionExists() {
 		UnionFind<Integer> uf = new UnionFind<Integer>();
 		uf.connect(1, 2);
-		Assert.assertTrue(uf.isConnected(1, 2));
-		Assert.assertTrue(uf.isConnected(2, 1));
+		Assert.assertTrue("Connection expected", uf.isConnected(1, 2));
+		Assert.assertTrue("Connection must be symmetric", uf.isConnected(2, 1));
+	}
+	
+	@Test
+	public void testWhenLoop() {
+		UnionFind<Integer> uf = new UnionFind<Integer>();
+		Integer x = 10;
+		Assert.assertTrue("An element is always connected to itself", uf.isConnected(x, x));
 	}
 	
 	@Test
@@ -63,12 +64,12 @@ public class UnionFindTest {
 		uf.connect(1, 2);
 		uf.connect(2, 3);
 		uf.connect(2, 4);
-		Assert.assertTrue(uf.isConnected(1, 2));
-		Assert.assertTrue(uf.isConnected(2, 1));
-		Assert.assertTrue(uf.isConnected(1, 4));
-		Assert.assertTrue(uf.isConnected(4, 1));
-		Assert.assertTrue(uf.isConnected(3, 4));
-		Assert.assertTrue(uf.isConnected(4, 3));
+		Assert.assertTrue("Connection expected", uf.isConnected(1, 2));
+		Assert.assertTrue("Connection must be symmetric", uf.isConnected(2, 1));
+		Assert.assertTrue("Connection expected", uf.isConnected(1, 4));
+		Assert.assertTrue("Connection must be symmetric", uf.isConnected(4, 1));
+		Assert.assertTrue("Connection expected", uf.isConnected(3, 4));
+		Assert.assertTrue("Connection must be symmetric", uf.isConnected(4, 3));
 	}
-
+	
 }
